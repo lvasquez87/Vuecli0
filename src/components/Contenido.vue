fa9f8b05ff95c7d46566c4be33a9302b1a90484d
lvasquez87 <ie.l3vm@gmail.com>
2020-04-24 21:38:40 -0400

    deleteapp01

--- src/components/Contenido.vue
+++ src/components/Contenido.vue
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <template>
     <div>
         <b-container fluid="md">
             
         </b-container>
     </div>
-=======
-<template>
-    <div>
-        <b-container fluid="md">
-            
-        </b-container>
-    </div>
->>>>>>> 22ca08237c80beb0bf3b2016185cb66777f7b9cd
 </template>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
     <div id="footerr">
         <b-navbar type="dark" variant="dark" class="p-0" fixed="bottom">
@@ -15,21 +14,3 @@ export default {
     name: 'footer'
 }
 </script>
-=======
-<template>
-    <div id="footerr">
-        <b-navbar type="dark" variant="dark" class="p-0" fixed="bottom">
-            <b-navbar-nav>
-                <b-nav-text class="m-0 p-0">2020</b-nav-text>
-            </b-navbar-nav>            
-        </b-navbar>
-        
-    </div>
-</template>
-
-<script>
-export default {
-    name: 'footer'
-}
-</script>
->>>>>>> 22ca08237c80beb0bf3b2016185cb66777f7b9cd
